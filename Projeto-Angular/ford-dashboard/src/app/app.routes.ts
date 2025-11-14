b64babb6d3538d9172e2fcf89e77cd5246cab20b
Renan Santos <88851654+RenanSantos01@users.noreply.github.com>
2025-11-14 16:57:37 -0300

    update

--- Projeto-Angular/ford-dashboard/src/app/app.routes.ts
+++ Projeto-Angular/ford-dashboard/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component'; // ✅ novo import
+import { DashboardComponent } from './dashboard/dashboard.component'; 
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent }, // ✅ nova rota
-  { path: '**', redirectTo: 'login' }
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]}, 
 ];
